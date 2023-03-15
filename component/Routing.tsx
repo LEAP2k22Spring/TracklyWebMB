8f2f724b253820bf0d985d1d31cafeef349f83ab
MBbold <103709596+MBbold@users.noreply.github.com>
2023-03-15 16:43:49 +0800

    routing turn off

--- component/Routing.tsx
+++ component/Routing.tsx
@@ -10,42 +10,42 @@ export const RoutingMachine = () => {
     iconSize: [20, 25],
   });
   useEffect(() => {
-    let marker1 = L.marker([47.91804573251184, 106.92599222374862], {
-      icon: chooseIcon,
-    }).addTo(map);
-    map.on("click", (e) => {
-      // L.marker([e.latlng.lat, e.latlng.lng]).addTo(map);
-      L.Routing.control({
-        waypoints: [
-          L.latLng(47.91804573251184, 106.92599222374862),
-          L.latLng(e.latlng.lat, e.latlng.lng),
-        ],
-        lineOptions: {
-          styles: [
-            {
-              color: "blue",
-              opacity: 0.6,
-              weight: 4,
-            },
-          ],
-        },
-        routeWhileDragging: false,
-        addWaypoints: false,
-        draggableWaypoints: false,
-        fitSelectedRoutes: true,
-        showAlternatives: true,
-      })
-        .on("routesfound", (e:any) => {
+    // let marker1 = L.marker([47.91804573251184, 106.92599222374862], {
+    //   icon: chooseIcon,
+    // }).addTo(map);
+    // map.on("click", (e) => {
+    //   // L.marker([e.latlng.lat, e.latlng.lng]).addTo(map);
+    //   L.Routing.control({
+    //     waypoints: [
+    //       L.latLng(47.91804573251184, 106.92599222374862),
+    //       L.latLng(e.latlng.lat, e.latlng.lng),
+    //     ],
+    //     lineOptions: {
+    //       styles: [
+    //         {
+    //           color: "blue",
+    //           opacity: 0.6,
+    //           weight: 4,
+    //         },
+    //       ],
+    //     },
+    //     routeWhileDragging: false,
+    //     addWaypoints: false,
+    //     draggableWaypoints: false,
+    //     fitSelectedRoutes: true,
+    //     showAlternatives: true,
+    //   })
+    //     .on("routesfound", (e:any) => {
           
-          e.routes[0].coordinates.forEach((c: { lat: number; lng: number; }, i: number) => {
+    //       e.routes[0].coordinates.forEach((c: { lat: number; lng: number; }, i: number) => {
             
-            setTimeout(() => {
-              marker1.setLatLng([c.lat, c.lng]);
-            }, 1000 * i);
-          });
-        })
-        .addTo(map);
-    });
+    //         setTimeout(() => {
+    //           marker1.setLatLng([c.lat, c.lng]);
+    //         }, 1000 * i);
+    //       });
+    //     })
+    //     .addTo(map);
+    // });
   }, []);
   return null;
 };
